3cdf0c957d17c419060f4ed2586579582b1868e4
Nadia Laban <labannadia5@gmail.com>
2023-10-23 01:08:03 +0300

    fix

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -166,9 +166,9 @@ export default {
                 m.text.toLowerCase().includes(this.search_query.toLowerCase())))
 
             if (this.current_clinic)
-                this.filtered_messages = this.messages.filter(m => !m.include_clinics && !m.exclude_clinics ||
-                    m.include_clinics && m.include_clinics.includes(this.current_clinic.id) ||
-                    m.exclude_clinics && !m.exclude_clinics.includes(this.current_clinic.id))
+                this.filtered_messages = this.filtered_messages.filter(m => !m.include_clinics && !m.exclude_clinics ||
+                    m.include_clinics && m.include_clinics.includes(this.current_clinic) ||
+                    m.exclude_clinics && !m.exclude_clinics.includes(this.current_clinic))
 
             this.filtered_messages = this.filtered_messages.sort((a, b) => ((a.title < b.title) ? -1 : ((a.title > b.title) ? 1 : 0)))
             this.$forceUpdate()
