057a7ec5413d9a2de85591d2c63c7d9f9dfe0baf
roctbb <roctbb@gmail.com>
2024-02-07 15:11:51 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 npm install
 sudo pip3 install --upgrade -r requirements.txt
 sudo apt-get install python3-cffi libcairo2 libpango-1.0-0 libpangocairo-1.0-0 libgdk-pixbuf2.0-0 libffi-dev shared-mime-info python3-setuptools python3-wheel build-essential python3-dev
+sudo cp library.ini /etc/uwsgi/apps/
 sudo cp agents_library.conf /etc/supervisor/conf.d/
 sudo cp agents_library_nginx.conf /etc/nginx/sites-enabled/
 sudo supervisorctl update
